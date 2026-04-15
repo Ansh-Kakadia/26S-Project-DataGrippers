b3a998ec93e05c289ee5aa058506fd167000d3d3
Ansh Kakadia <kakadia.an@northeastern.edu>
2026-04-15 15:45:47 -0400

    changed parameterization

--- api/backend/blueprints/notifications.py
+++ api/backend/blueprints/notifications.py
@@ -2,32 +2,31 @@ from flask import Blueprint, jsonify, request, current_app
 from backend.db_connection import get_db
 from mysql.connector import Error
 
-notifications = Blueprint("analytics", __name__)
+notifications = Blueprint("notifications", __name__)
 
 
-## Send league wide notification
-@notifications.route("/notification", methods=["GET"])
-def get_league_wide_notifications():
-    cursor = get_db().cursor(dictionary=True)
-    query = 'SELECT * FROM Notifications WHERE 1=1'
+## Get league wide notification
+@notifications.route("/notification/<int:league_id>", methods=["GET"])
+def get_league_wide_notifications(league_id: int):
     
+    cursor = get_db().cursor(dictionary=True)
+    query = f"SELECT * FROM Notifications WHERE league_id = {league_id}"
+
     try:
         current_app.logger.info("GET /notification")
-        league_id = request.args.get("league_id")
-        if league_id:
-            query += f' AND league_id = {league_id}'
-        else: 
-            return jsonify({"error": "%s is required"}), 400
-            
         cursor.execute(query, (league_id,))
         results = cursor.fetchall()
-        
-        current_app.logger.info(f'Retrieved {len(results)} data results for notifications')
+
+        current_app.logger.info(
+            f"Retrieved {len(results)} data results for notifications"
+        )
         return jsonify(results), 200
 
     except Error as e:
-        current_app.logger.error(f'Database error in get_league_wide_notifications: {e}')
+        current_app.logger.error(
+            f"Database error in get_league_wide_notifications: {e}"
+        )
         return jsonify({"error": str(e)}), 500
     finally:
-        cursor.close()    
-    
+        cursor.close()
+
--- api/backend/blueprints/players.py
+++ api/backend/blueprints/players.py
@@ -1,33 +1,36 @@
-from flask import Blueprint, jsonify, request, current_app
+from flask import Blueprint, jsonify, current_app, request
 from backend.db_connection import get_db
 from mysql.connector import Error
 
-player = Blueprint("analytics", __name__)
+players = Blueprint("players", __name__)
 
 
-## Send league wide notification
-@player.route("/player/notifications", methods=["GET"])
-def get_unread_player_notifications():
+## Get unread notifications for a specific player
+@players.route("/players/<int:player_id>/notifications", methods=["GET"])
+def get_unread_player_notifications(player_id):
     cursor = get_db().cursor(dictionary=True)
-    query = 'SELECT * FROM Notifications WHERE is_read = False'
-    
+    unread_only = request.args.get("unread_only", "false").lower() == "true"
+    query = "SELECT * FROM Notifications WHERE player_id = %s"
+
+    if unread_only:
+        query += " AND NOT is_read"
+
     try:
-        current_app.logger.info("GET /player/notifications")
-        player_id = request.args.get("player_id")
-        if player_id:
-            query += f' AND player_id = {player_id}'
-        else: 
-            return jsonify({"error": "%s is required"}), 400
-            
+        current_app.logger.info(
+            f"GET /players/{player_id}/notifications?unread_only={unread_only}"
+        )
         cursor.execute(query, (player_id,))
         results = cursor.fetchall()
-        
-        current_app.logger.info(f'Retrieved {len(results)} data results for notifications')
+
+        current_app.logger.info(
+            f"Retrieved {len(results)} data results for notifications"
+        )
         return jsonify(results), 200
 
     except Error as e:
-        current_app.logger.error(f'Database error in get_unread_player_notifications: {e}')
+        current_app.logger.error(
+            f"Database error in get_unread_player_notifications: {e}"
+        )
         return jsonify({"error": str(e)}), 500
     finally:
-        cursor.close()    
-    
+        cursor.close()
